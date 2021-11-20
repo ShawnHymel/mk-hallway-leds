33fd110b48ba31b3563eec29911bb87bba58fb3f
Shawn Hymel <hymelsr@vt.edu>
2021-11-20 09:49:02 -0600

    Added simple PIO WS2812b example and links in READMS

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mystic Krewe - Hallway LEDs
+
+Six strips of WS2812b LEDs (NeoPixels) line the entrance of the PinChurch and respond to visitors.
+
+## References and Guides
+
+ * [Adafruit: WS2812b with PIO and shift register example](https://learn.adafruit.com/neopio-drive-lots-of-leds-with-raspberry-pi-pico/overview)
+ * [Core Electronics: WS2812b with PIO example](https://core-electronics.com.au/tutorials/how-to-use-ws2812b-rgb-leds-with-raspberry-pi-pico.html)
+ * [rp2 MicroPython API](https://docs.micropython.org/en/latest/library/rp2.html)
---
+++ tests/pio-ws2812b-test-01.py
@@ -0,0 +1,49 @@
+# Example using PIO to drive a set of WS2812 LEDs.
+
+import array, time
+from machine import Pin
+import rp2
+
+# Configure the number of WS2812 LEDs.
+NUM_LEDS = 2
+PIN_NUM = 15
+brightness = 1.0
+
+@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
+def ws2812():
+    T1 = 2
+    T2 = 5
+    T3 = 3
+    wrap_target()
+    label("bitloop")
+    out(x, 1)               .side(0)    [T3 - 1]
+    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]
+    jmp("bitloop")          .side(1)    [T2 - 1]
+    label("do_zero")
+    nop()                   .side(0)    [T2 - 1]
+    wrap()
+
+
+# Create the StateMachine with the ws2812 program, outputting on pin
+sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(PIN_NUM))
+
+# Start the StateMachine, it will wait for data on its FIFO.
+sm.active(1)
+
+# Array to hold pixel color values
+ar = array.array("I", [0 for _ in range(NUM_LEDS)])
+
+# Push color values to state machine
+def show(fb):
+    for i in range(NUM_LEDS):
+        sm.put(fb[i], 8)
+
+# Test: one pixel on
+while True:
+    r = int(255 * brightness)
+    g = int(127 * brightness)
+    b = int(0 * brightness)
+    print("Writing:", r, g, b)
+    ar[1] = (g << 16) + (r << 8) + b
+    show(ar)
+    time.sleep(0.1)
